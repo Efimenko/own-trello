f23bb5b8aa03b993b7fbab13047c214c29cf48f0
efimenko bohdan <bogdane@weblium.com>
2019-12-13 19:45:33 +0200

    move client sources to separate folder

--- client/src/components/group-list/group-item.js
+++ client/src/components/group-list/group-item.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {PropTypes} from 'prop-types'
 import {AddTaskForm} from './add-task-form'
 import {TaskList} from './task-list'
-import {getTasksByParentId} from 'shared/utils'
+import {getTasksByParentId} from 'utils'
 
 export const GroupItem = ({id, title, tasks}) => (
   <section>
