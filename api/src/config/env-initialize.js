dd3d53a6508b6471092fde2bf14ea3fc5f6214c8
Yefimenko Bohdan <bohdan@seekingalpha.com>
2020-02-14 09:47:26 +0200

    fixed miss run donenv config if .env file exists

--- api/src/config/env-initialize.js
+++ api/src/config/env-initialize.js
@@ -3,20 +3,24 @@ const dotenv = require('dotenv')
 const {generateTokenAndWriteToFile} = require('./generate-env')
 const {logger} = require('../logger')
 
+const runDotenvConfig = () => {
+  const result = dotenv.config()
+
+  if (result.error) {
+    logger.error({err: result.error})
+  } else {
+    logger.info('.env file was successful initialized')
+  }
+}
+
 const envInitialize = () => {
   const path = `${process.cwd()}/.env`
   try {
     if (!fs.existsSync(path)) {
       logger.info("Can't find .env file localy. Start creating file...")
-      generateTokenAndWriteToFile().then(() => {
-        const result = dotenv.config()
-
-        if (result.error) {
-          logger.error({err: result.error})
-        } else {
-          logger.info('.env file was successful initialized')
-        }
-      })
+      generateTokenAndWriteToFile().then(runDotenvConfig)
+    } else {
+      runDotenvConfig()
     }
   } catch (err) {
     logger.error({err})
