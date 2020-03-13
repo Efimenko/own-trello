a31da9ec617ee58a04ab42bf6743273ba56b8cc0
Yefimenko Bohdan <bohdan@seekingalpha.com>
2020-03-13 08:55:18 +0200

    migrate aliase pages to __pages

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
-import {AuthPage, GroupsPage} from 'pages'
+import {AuthPage, GroupsPage} from '__pages'
 import {Authentication} from './authentication'
 import {RedirectsResolver} from './redirects-resolver'
 
