0f0644ab55754eec89c71271c88b618f09c0fd5d
Yefimenko Bohdan <bohdan@seekingalpha.com>
2020-03-13 11:34:23 +0200

    fixed prettier errors after autoreplace aliases

--- client/src/store/epics/user/logout.js
+++ client/src/store/epics/user/logout.js
@@ -2,7 +2,11 @@ import {ofType} from 'redux-observable'
 import {switchMap} from 'rxjs/operators'
 
 import {userTypes} from '__store/actions/types'
-import {userActions, groupsActions, tasksActions} from '__store/actions/creators'
+import {
+  userActions,
+  groupsActions,
+  tasksActions,
+} from '__store/actions/creators'
 import {concat, of} from 'rxjs'
 
 export const logOutUserEpic = (action$) => {
