dd8274848edda049d20476803ecb41ae3de973e3
Yefimenko Bohdan <bohdan@seekingalpha.com>
2020-03-13 08:02:19 +0200

    added errors display in SignInForm component

--- client/src/pages/auth/sign-in-form.js
+++ client/src/pages/auth/sign-in-form.js
@@ -1,13 +1,18 @@
 import React, {useState} from 'react'
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 
 import {userActions} from 'store/actions/creators'
 
 export const SignInForm = () => {
   const [emailValue, setEmailValue] = useState('')
   const [passwordValue, setPasswordValue] = useState('')
+
   const [loading, setLoading] = useState(false)
+
   const dispatch = useDispatch()
+
+  const errorsOwner = 'SignInForm'
+  const errors = useSelector((state) => state.errors[errorsOwner]) || []
 
   const handleSubmitForm = (event) => {
     event.preventDefault()
@@ -16,39 +21,48 @@ export const SignInForm = () => {
       userActions.login({
         email: emailValue,
         password: passwordValue,
-        errorsOwner: 'SignInForm',
+        errorsOwner,
       })
     )
   }
 
   return (
-    <form onSubmit={handleSubmitForm}>
-      <label htmlFor="sing-in-email">Email</label>
-      <br />
-      <input
-        value={emailValue}
-        type="email"
-        placeholder="Type your email"
-        name="email"
-        onChange={(event) => setEmailValue(event.target.value)}
-        id="sign-in-email"
-        required
-      />
-      <br />
-      <label htmlFor="sign-in-password">Password</label>
-      <br />
-      <input
-        value={passwordValue}
-        type="password"
-        placeholder="Type your password"
-        name="password"
-        onChange={(event) => setPasswordValue(event.target.value)}
-        id="sign-in-password"
-        required
-      />
-      <button type="submit" disabled={loading}>
-        Login
-      </button>
-    </form>
+    <>
+      {/* TODO: Create error component */}
+      {Boolean(errors.length) &&
+        errors.map(({message}) => (
+          <div key={message} style={{color: 'red'}}>
+            {message}
+          </div>
+        ))}
+      <form onSubmit={handleSubmitForm}>
+        <label htmlFor="sing-in-email">Email</label>
+        <br />
+        <input
+          value={emailValue}
+          type="email"
+          placeholder="Type your email"
+          name="email"
+          onChange={(event) => setEmailValue(event.target.value)}
+          id="sign-in-email"
+          required
+        />
+        <br />
+        <label htmlFor="sign-in-password">Password</label>
+        <br />
+        <input
+          value={passwordValue}
+          type="password"
+          placeholder="Type your password"
+          name="password"
+          onChange={(event) => setPasswordValue(event.target.value)}
+          id="sign-in-password"
+          required
+        />
+        <button type="submit" disabled={loading}>
+          Login
+        </button>
+      </form>
+    </>
   )
 }
