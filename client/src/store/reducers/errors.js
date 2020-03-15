90732af9ca9384fc66e6562f97de68d8e1ac8879
Yefimenko Bohdan <bohdan@seekingalpha.com>
2020-03-15 16:59:58 +0200

    add flow types to errors reducer

--- client/src/store/reducers/errors.js
+++ client/src/store/reducers/errors.js
@@ -1,7 +1,32 @@
-import {errorsTypes} from '../actions/types'
-const defaultState = {}
+// @flow
 
-export const errors = (state = defaultState, {type, payload}) => {
+import {errorsTypes} from '../actions/types'
+
+type IdT = symbol
+
+type ErrorT = {
+  id: IdT,
+}
+
+type StateT = {
+  [string]: $ReadOnlyArray<ErrorT>,
+}
+
+type ReducerOptionT = {
+  type: string,
+  payload: {
+    errorId: IdT | $ReadOnlyArray<IdT>,
+    errors: $ReadOnlyArray<ErrorT>,
+    errorsOwner: string,
+  },
+}
+
+const defaultState: StateT = {}
+
+export const errors = (
+  state: StateT = defaultState,
+  {type, payload}: ReducerOptionT
+) => {
   switch (type) {
     case errorsTypes.ADD:
       return {
