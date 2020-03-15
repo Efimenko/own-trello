3a5537bf463c0d9ae849ef5f0459f23292d5f1d4
Yefimenko Bohdan <bohdan@seekingalpha.com>
2020-03-15 18:20:53 +0200

    add types in errors store actions

--- client/src/store/actions/creators/errors/index.js
+++ client/src/store/actions/creators/errors/index.js
@@ -1,12 +1,50 @@
+// @flow
+
 import {errorsTypes} from '../../types'
+import type {
+  ErrorsIdT,
+  ErrorsT,
+  ErrorsOwnerT,
+  ErrorsAddActionT,
+  ErrorsRemoveActionT,
+} from '../../types'
+
+type RemoveParamsT = {
+  errorsId: ErrorsIdT,
+  errorsOwner: ErrorsT,
+}
+
+type RemoveResultT = {
+  type: ErrorsRemoveActionT,
+  payload: {
+    errorsId: ErrorsIdT,
+    errorsOwner: ErrorsOwnerT,
+  },
+}
+
+type AddParamsT = {
+  errors: $ReadOnlyArray<ErrorsT>,
+  errorsOwner: ErrorsT,
+}
+
+type AddResultT = {
+  type: ErrorsAddActionT,
+  payload: {
+    errors: $ReadOnlyArray<ErrorsT>,
+    errorsOwner: ErrorsT,
+  },
+}
 
 /* Remove error action creator */
-export const remove = ({errorsId, errorsOwner}) => ({
+export const remove = ({
+  errorsId,
+  errorsOwner,
+}: RemoveParamsT): RemoveResultT => ({
   type: errorsTypes.REMOVE,
   payload: {errorsId, errorsOwner},
 })
 
-export const add = ({errors, errorsOwner}) => ({
+export const add = ({errors, errorsOwner}: AddParamsT): AddResultT => ({
   type: errorsTypes.ADD,
   payload: {errors, errorsOwner},
 })
